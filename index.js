c2e1d800a400b2c6f2928a83dc3e41848a10e7e9
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-03-13 11:39:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   var empty = [];
   for(i = 0; i < musicians.length; i++){
-
+    if(i===0){
+      return empty.push("${musicians[0]) + plays + ${instruments[0]} ");
+    }
   }
 }
